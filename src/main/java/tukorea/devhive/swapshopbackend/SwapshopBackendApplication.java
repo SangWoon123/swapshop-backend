ba045f552033ea9389bcbd860a37b47263b25a0c
SangWoon123 <alrax3574@naver.com>
2023-07-05 00:22:35 +0900

    초기설정

---
+++ src/main/java/tukorea/devhive/swapshopbackend/SwapshopBackendApplication.java
@@ -0,0 +1,13 @@
+package tukorea.devhive.swapshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapshopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapshopBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tukorea/devhive/swapshopbackend/SwapshopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tukorea.devhive.swapshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapshopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
