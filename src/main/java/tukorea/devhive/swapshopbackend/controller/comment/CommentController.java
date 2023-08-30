0c0ff8805e2a777ca960f311352fb310a88c2072
jgmoon <6813821@tukorea.ac.kr>
2023-08-31 03:05:29 +0900

    [ADD] swagger 적용

--- src/main/java/tukorea/devhive/swapshopbackend/controller/comment/CommentController.java
+++ src/main/java/tukorea/devhive/swapshopbackend/controller/comment/CommentController.java
@@ -1,5 +1,7 @@
 package tukorea.devhive.swapshopbackend.controller.comment;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +15,12 @@ import java.util.List;
 @RequestMapping("/post")
 @CrossOrigin(origins = "http://localhost:3000")
 @RequiredArgsConstructor
-
+@Tag(name = "Comment", description = "댓글 API Document")
 public class CommentController {
     private final CommentService commentService;
 
     // 댓글 목록 조회
+    @Operation(summary = "댓글 목록", description = "댓글 목록을 조회합니다.")
     @GetMapping("/{postId}/comment")
     public ResponseEntity<List<CommentDTO>> comments(@PathVariable Long postId) {
 
@@ -27,6 +30,7 @@ public class CommentController {
     }
 
     // 생성
+    @Operation(summary = "댓글 생성", description = "댓글을 생성합니다.")
     @PostMapping("/{postId}/comment")
     public ResponseEntity<CommentDTO> create(@PathVariable Long postId, @RequestBody CommentDTO dto) {
 
@@ -36,6 +40,7 @@ public class CommentController {
     }
 
     // 대댓글 생성
+    @Operation(summary = "대댓글 생성", description = "대댓글을 생성합니다.")
     @PostMapping("/{postId}/comment/{parentId}")
     public ResponseEntity<CommentDTO> createReply(
             @PathVariable Long postId,
@@ -47,8 +52,8 @@ public class CommentController {
         return ResponseEntity.status(HttpStatus.OK).body(createDto);
     }
 
-
     // 수정
+    @Operation(summary = "댓글 수정", description = "댓글을 수정합니다.")
     @PatchMapping("/{postId}/comment/{id}")
     public ResponseEntity<CommentDTO> update(@PathVariable Long id, @RequestBody CommentDTO dto) {
 
@@ -58,6 +63,7 @@ public class CommentController {
     }
 
     // 삭제
+    @Operation(summary = "댓글 삭제", description = "댓글을 삭제합니다.")
     @DeleteMapping("/{postId}/comment/{id}")
     public ResponseEntity<CommentDTO> delete(@PathVariable("id") Long id) {
 
