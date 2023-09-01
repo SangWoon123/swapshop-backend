c79a9eb4ec923f5f2edc8b2c0310ed4b18007c6d
SangWoon123 <alrax3574@naver.com>
2023-09-01 20:36:57 +0900

    [UPDATE] 찜하기 등록 프론트측에서 못받는 오류 수정

--- src/main/java/tukorea/devhive/swapshopbackend/controller/favorite/FavoriteController.java
+++ src/main/java/tukorea/devhive/swapshopbackend/controller/favorite/FavoriteController.java
@@ -11,6 +11,7 @@ import tukorea.devhive.swapshopbackend.model.dto.post.FavoriteDTO;
 import tukorea.devhive.swapshopbackend.service.favorite.FavoriteService;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,9 +23,21 @@ public class FavoriteController {
 
     // 게시물 찜하기, 찜 취소하기
     // 서비스코드에는 찜하기 여부에 따라 찜하거나 찜하기를 취소하는 로직이 포함되어있음
-    @PostMapping("/{postId}")
-    public ResponseEntity<?> addMemberFavorite(@PathVariable(name = "postId") Long postId, @AuthenticationPrincipal LoginDTO loginDTO){
-        return favoriteService.toggleMemberFavorite(postId, loginDTO.getUserId());
+
+    // ver 1.0 에서 API 가 POST인데 Body에 아무것도 없이 전달하는 바람에 프론트측에서 받으면 오류가 발생하는 상황이었음
+//    @PostMapping("/{postId}")
+//    public ResponseEntity<?> addMemberFavorite(@PathVariable(name = "postId") Long postId, @AuthenticationPrincipal LoginDTO loginDTO){
+//        return favoriteService.toggleMemberFavorite(postId, loginDTO.getUserId());
+//    }
+
+    // RequestBody Map<String,Long> 으로 데이터 전달 받음 (아래 예시)
+    /* {
+        "postId":1
+        }
+    */
+    @PostMapping()
+    public ResponseEntity<?> addMemberFavorite(@RequestBody Map<String,Long> postId, @AuthenticationPrincipal LoginDTO loginDTO){
+        return favoriteService.toggleMemberFavorite(postId.get("postId"), loginDTO.getUserId());
     }
 
     // 유저가 찜한 게시물 불러오기
